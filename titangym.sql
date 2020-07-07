bda74a89fea46f9f8351d34608615f459d3bda6f
Nilesh Joshi <55633122+nilesh-j98@users.noreply.github.com>
2020-07-07 21:21:16 +0530

    Add files via upload

---
+++ titangym.sql
@@ -0,0 +1,357 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.0.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Nov 28, 2019 at 06:53 AM
+-- Server version: 10.4.6-MariaDB
+-- PHP Version: 7.3.9
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `titangym`
+--
+
+DELIMITER $$
+--
+-- Procedures
+--
+CREATE DEFINER=`root`@`localhost` PROCEDURE `insertData` (IN `in_username` VARCHAR(40), IN `in_gender` VARCHAR(8), IN `in_mobile` VARCHAR(20), IN `in_email` VARCHAR(20), IN `in_dob` VARCHAR(10), IN `in_joining_date` VARCHAR(10), IN `in_userid` VARCHAR(20))  BEGIN
+INSERT INTO users(username, gender, mobile, email, dob, joining_date, userid) VALUES(in_username,in_gender,in_mobile,in_email,in_dob,in_joining_date,in_userid);
+END$$
+
+DELIMITER ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `address`
+--
+
+CREATE TABLE `address` (
+  `id` varchar(20) NOT NULL,
+  `streetName` varchar(40) NOT NULL,
+  `state` varchar(15) NOT NULL,
+  `city` varchar(15) NOT NULL,
+  `zipcode` varchar(20) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 ROW_FORMAT=DYNAMIC;
+
+--
+-- Dumping data for table `address`
+--
+
+INSERT INTO `address` (`id`, `streetName`, `state`, `city`, `zipcode`) VALUES
+('1529336794', '30 OM PG', 'Karnataka', 'Bangalore', '560090'),
+('1572760056', 'Soldevanhalli', 'Karnataka', 'Bangalore', '560091');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admin`
+--
+
+CREATE TABLE `admin` (
+  `username` varchar(20) NOT NULL,
+  `pass_key` varchar(20) NOT NULL,
+  `securekey` varchar(20) NOT NULL,
+  `Full_name` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 ROW_FORMAT=DYNAMIC;
+
+--
+-- Dumping data for table `admin`
+--
+
+INSERT INTO `admin` (`username`, `pass_key`, `securekey`, `Full_name`) VALUES
+('admin3', 'nb123', 'admin1', 'Titanium Gym');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `enrolls_to`
+--
+
+CREATE TABLE `enrolls_to` (
+  `et_id` int(5) NOT NULL,
+  `pid` varchar(8) NOT NULL,
+  `uid` varchar(20) NOT NULL,
+  `paid_date` varchar(15) DEFAULT NULL,
+  `expire` varchar(15) DEFAULT NULL,
+  `renewal` varchar(15) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 ROW_FORMAT=DYNAMIC;
+
+--
+-- Dumping data for table `enrolls_to`
+--
+
+INSERT INTO `enrolls_to` (`et_id`, `pid`, `uid`, `paid_date`, `expire`, `renewal`) VALUES
+(1, 'POQKJC', '1529336794', '2018-06-18', '2018-07-18', 'no'),
+(3, 'POQKJC', '1529336794', '2019-11-03', '2019-12-03', 'no'),
+(4, 'POQKJC', '1572760056', '2019-11-03', '2019-12-03', 'no'),
+(5, 'JIMYKQ', '1572760056', '2019-11-03', '2020-01-03', 'no'),
+(12, 'POQKJC', '1529336794', '2019-11-22', '2019-12-22', 'no'),
+(13, 'IPUQFL', '1572760056', '2019-11-27', '2020-11-27', 'yes'),
+(16, 'POQKJC', '1529336794', '2019-11-28', '2019-12-28', 'yes');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `health_status`
+--
+
+CREATE TABLE `health_status` (
+  `hid` int(5) NOT NULL,
+  `calorie` varchar(8) DEFAULT NULL,
+  `height` varchar(8) DEFAULT NULL,
+  `weight` varchar(8) DEFAULT NULL,
+  `fat` varchar(8) DEFAULT NULL,
+  `remarks` varchar(200) DEFAULT NULL,
+  `uid` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 ROW_FORMAT=DYNAMIC;
+
+--
+-- Dumping data for table `health_status`
+--
+
+INSERT INTO `health_status` (`hid`, `calorie`, `height`, `weight`, `fat`, `remarks`, `uid`) VALUES
+(1, '', '', '', '', '', '1529336794'),
+(3, '', '', '', '', '', '1572760056');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `logs`
+--
+
+CREATE TABLE `logs` (
+  `id` int(11) NOT NULL,
+  `userid` varchar(20) NOT NULL,
+  `action` varchar(40) NOT NULL,
+  `date` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `logs`
+--
+
+INSERT INTO `logs` (`id`, `userid`, `action`, `date`) VALUES
+(15, '1574861601', 'Data Inserted Succesfully', '2019-11-27 19:04:19'),
+(16, '1574861660', 'Data Inserted Succesfully', '2019-11-27 19:05:51'),
+(17, '1574861601', 'User Deleted Succesfully', '2019-11-27 19:06:31'),
+(18, '1574861660', 'User Deleted Succesfully', '2019-11-27 19:06:33'),
+(19, '1572760056', 'Data Updated Succesfully', '2019-11-28 08:38:41'),
+(20, '1572760056', 'Data Updated Succesfully', '2019-11-28 08:38:47'),
+(21, '1574911513', 'Data Inserted Succesfully', '2019-11-28 08:56:12'),
+(22, '1574911513', 'User Deleted Succesfully', '2019-11-28 08:57:11'),
+(23, '1529336794', 'Data Updated Succesfully', '2019-11-28 08:58:23');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `plan`
+--
+
+CREATE TABLE `plan` (
+  `pid` varchar(8) NOT NULL,
+  `planName` varchar(20) NOT NULL,
+  `description` varchar(200) NOT NULL,
+  `validity` varchar(20) NOT NULL,
+  `amount` int(10) NOT NULL,
+  `active` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 ROW_FORMAT=DYNAMIC;
+
+--
+-- Dumping data for table `plan`
+--
+
+INSERT INTO `plan` (`pid`, `planName`, `description`, `validity`, `amount`, `active`) VALUES
+('IPUQFL', 'Yearly Plan', 'Unlocks the entire app and all new gym plans released for 12 months! Lowest priced only 6000 per year!!', '12', 6000, 'yes'),
+('JIMYKQ', 'demo', 'abc', '2', 1000, 'no'),
+('POQKJC', 'Monthly Plan', 'A monthly subscription that unlocks the entire Gym Plan app and coach support on chat.', '1', 600, 'yes');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `timetable`
+--
+
+CREATE TABLE `timetable` (
+  `tid` int(12) NOT NULL,
+  `tname` varchar(45) DEFAULT NULL,
+  `day1` varchar(200) DEFAULT NULL,
+  `day2` varchar(200) DEFAULT NULL,
+  `day3` varchar(200) DEFAULT NULL,
+  `day4` varchar(200) DEFAULT NULL,
+  `day5` varchar(200) DEFAULT NULL,
+  `day6` varchar(200) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 ROW_FORMAT=DYNAMIC;
+
+--
+-- Dumping data for table `timetable`
+--
+
+INSERT INTO `timetable` (`tid`, `tname`, `day1`, `day2`, `day3`, `day4`, `day5`, `day6`) VALUES
+(2, 'Workout', 'Flat bench barbell press - 4 sets of 8-12 reps\r\nIncline dumbell press - 4 sets of 8-12 reps\r\nIncline dumbell flyers - 3 set of 10 reps\r\nCable crossovers - 3 sets of 15 reps\r\nPush-ups - 4sets', 'Seated dumbell shoulder press -4 sets of 12 reps\r\nStanding barbell military press - 4 sets of 10-12 reps\r\nDumbbell lateral raises - 4 sets of 12 reps', 'Barbell squats - 4 sets of 8-10 reps\r\nHeck squats - 4 sets of 10 reps\r\nLeg press machine -3 sets of 10 reps', 'Chin-ups - 4 sets of 10 reps\r\nWide grip lat pull-downs - 4 sets of 12 reps\r\nClose grip lat pull downs - 4 set of 12 reps\r\nDumbbell rows - 4 sets of 8-10 reps', 'Double arm dumbbell curls - 4 sets of 10-12 reps\r\nEZ bar curls - 4 sets of 10-12 reps\r\nPreacher curl machine - 4 sets of 12 reps\r\nTriceps rope machine - 4 sets of 15 reps', '60 minute of steady state cardio..');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `userid` varchar(20) NOT NULL,
+  `username` varchar(40) NOT NULL,
+  `gender` varchar(8) NOT NULL,
+  `mobile` varchar(20) NOT NULL,
+  `email` varchar(20) NOT NULL,
+  `dob` varchar(10) NOT NULL,
+  `joining_date` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 ROW_FORMAT=DYNAMIC;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`userid`, `username`, `gender`, `mobile`, `email`, `dob`, `joining_date`) VALUES
+('1529336794', 'Nilesh Joshi', 'Male', '9108387634', 'nileshjoshi800@gmail', '1998-11-19', '2019-11-01'),
+('1572760056', 'Gyan Punj', 'Male', '123456789', 'gyanp@xyz.com', '2000-03-04', '2019-11-03');
+
+--
+-- Triggers `users`
+--
+DELIMITER $$
+CREATE TRIGGER `deleteUser` BEFORE DELETE ON `users` FOR EACH ROW INSERT INTO logs VALUES(null, OLD.userid, "User Deleted Succesfully", NOW() )
+$$
+DELIMITER ;
+DELIMITER $$
+CREATE TRIGGER `insertUser` AFTER INSERT ON `users` FOR EACH ROW INSERT INTO logs VALUES(null, NEW.userid, "Data Inserted Succesfully", NOW())
+$$
+DELIMITER ;
+DELIMITER $$
+CREATE TRIGGER `updateUser` AFTER UPDATE ON `users` FOR EACH ROW INSERT INTO logs VALUES(null, NEW.userid, "Data Updated Succesfully", NOW() )
+$$
+DELIMITER ;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `address`
+--
+ALTER TABLE `address`
+  ADD KEY `userID` (`id`) USING BTREE;
+
+--
+-- Indexes for table `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`username`) USING BTREE;
+
+--
+-- Indexes for table `enrolls_to`
+--
+ALTER TABLE `enrolls_to`
+  ADD PRIMARY KEY (`et_id`) USING BTREE,
+  ADD KEY `user_ID` (`uid`) USING BTREE,
+  ADD KEY `plan_ID_idx` (`pid`) USING BTREE;
+
+--
+-- Indexes for table `health_status`
+--
+ALTER TABLE `health_status`
+  ADD PRIMARY KEY (`hid`) USING BTREE,
+  ADD KEY `userID_idx` (`uid`) USING BTREE;
+
+--
+-- Indexes for table `logs`
+--
+ALTER TABLE `logs`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `plan`
+--
+ALTER TABLE `plan`
+  ADD PRIMARY KEY (`pid`) USING BTREE,
+  ADD KEY `pid` (`pid`) USING BTREE;
+
+--
+-- Indexes for table `timetable`
+--
+ALTER TABLE `timetable`
+  ADD PRIMARY KEY (`tid`) USING BTREE;
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`userid`) USING BTREE,
+  ADD UNIQUE KEY `email` (`email`) USING BTREE,
+  ADD KEY `userid` (`userid`) USING BTREE;
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `enrolls_to`
+--
+ALTER TABLE `enrolls_to`
+  MODIFY `et_id` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+
+--
+-- AUTO_INCREMENT for table `health_status`
+--
+ALTER TABLE `health_status`
+  MODIFY `hid` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+
+--
+-- AUTO_INCREMENT for table `logs`
+--
+ALTER TABLE `logs`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
+
+--
+-- AUTO_INCREMENT for table `timetable`
+--
+ALTER TABLE `timetable`
+  MODIFY `tid` int(12) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `address`
+--
+ALTER TABLE `address`
+  ADD CONSTRAINT `userID` FOREIGN KEY (`id`) REFERENCES `users` (`userid`) ON DELETE CASCADE ON UPDATE NO ACTION;
+
+--
+-- Constraints for table `enrolls_to`
+--
+ALTER TABLE `enrolls_to`
+  ADD CONSTRAINT `plan_ID` FOREIGN KEY (`pid`) REFERENCES `plan` (`pid`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `user_ID` FOREIGN KEY (`uid`) REFERENCES `users` (`userid`) ON DELETE CASCADE ON UPDATE NO ACTION;
+
+--
+-- Constraints for table `health_status`
+--
+ALTER TABLE `health_status`
+  ADD CONSTRAINT `uID` FOREIGN KEY (`uid`) REFERENCES `users` (`userid`) ON DELETE CASCADE ON UPDATE NO ACTION;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
